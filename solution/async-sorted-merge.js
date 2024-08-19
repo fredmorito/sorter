8760c1c9d733004d4caecf811237503f205353af
ThisIsFredy <10052740+fredmorito@users.noreply.github.com>
2024-08-19 12:12:30 -0500

    Asynchronous sort working. Recursion in progress.

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,57 +1,63 @@
 "use strict";
 
+const bluebird = require("bluebird");
+
 const addLog = function (array, newLog, indexToCompareAgainst) {
+    
+    if (Boolean(array[indexToCompareAgainst]) === false) {
+        array.unshift(newLog);
+        
+        return array;
+    }
+    
+    if (newLog.date > array[indexToCompareAgainst].date) {
+        array.splice((indexToCompareAgainst + 1), 0, newLog);
+        
+        return array;
+    }
+    
+    return addLog(array, newLog, (indexToCompareAgainst - 1));
+};
 
-  if (Boolean(array[indexToCompareAgainst]) === false) {
-      array.unshift(newLog);
+const processPromises = function (sources) {
+    
+    const promises = sources.map(function(eachSource) {
+        return eachSource.popAsync();
+    });
+    
+    return Promise.all(promises)
+    .then(partialLogs => partialLogs.filter(eachLog => eachLog !== false))
+    .then(results => ({ results, reducedSources: sources }))
+    .then(({ results, reducedSources }) => {
+        
+        const logs = bluebird.reduce(results, (acc, curr) => {
+            
+            if (acc.length === 0) {
+                return [curr];
+            }
+            
+            return addLog(acc, curr, acc.length - 1);
+        }, []);
 
-      return array;
-  }
-
-  if (newLog.date > array[indexToCompareAgainst].date) {
-      array.splice((indexToCompareAgainst + 1), 0, newLog);
-
-      return array;
-  }
-
-  return addLog(array, newLog, (indexToCompareAgainst - 1));
+        return logs;
+    });
 };
 
 module.exports = (logSources, printer) => {
-  
-  return new Promise(async (resolve, reject) => {
-
-    let logs = [];
-
-    while (logSources.length > 0) {
-
-        for (let sourceIndex = 0; sourceIndex < logSources.length; sourceIndex++) {
-
-            const currentSource = logSources[sourceIndex];
-            const sourceLog = await currentSource.popAsync();
-
-            if (logs.length === 0) {
-                logs = [sourceLog];
-                break;
+    
+    return new Promise(async (resolve) => {
+        
+        processPromises(logSources)
+        .then(logs => {
+            // The Printer can't be called on every iteration because logs can be resorted at any moment
+            // Calling just to check the sorting
+            for (let counter = 0; counter < logs.length; counter++) {
+                printer.print(logs[counter]);
             }
-
-            if (sourceLog === false) { // No need to check anymore
-                logSources.splice(sourceIndex, 1);
-                break;
-            }
-
-            logs = addLog(logs, sourceLog, logs.length - 1);
-        }
-    }
-
-    // The Printer can't be called on every iteration because logs can be resorted at any moment
-    // Calling just to check the sorting
-    for (let counter = 0; counter < logs.length; counter++) {
-        printer.print(logs[counter]);
-    }
-
-    printer.done();
-
-    resolve(console.log("Async sort complete."));
-  });
+            
+            printer.done();
+            
+            resolve(console.log("Async sort complete."));
+        });
+    });
 };
