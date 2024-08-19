97d0f3159c3a5a1748608cb90e03ab64f24b6c7c
ThisIsFredy <10052740+fredmorito@users.noreply.github.com>
2024-08-18 21:50:09 -0500

    Reading all logs. Sorting WIP.

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -3,5 +3,26 @@
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
-  return console.log("Sync sort complete.");
+
+    const logs = [];
+
+    while (logSources.length > 0) {
+
+        for (let sourceIndex = 0; sourceIndex < logSources.length; sourceIndex++) {
+
+            const currentSource = logSources[sourceIndex];
+            const sourceLog = currentSource.pop();
+
+            if (sourceLog === false) {
+                logSources.splice(sourceIndex, 1);
+                break;
+            }
+
+            logs.push(sourceLog);
+        }
+    }
+
+    console.table(logs);
+
+    return console.log("Sync sort complete.");
 };
